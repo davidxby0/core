52cafa1bcea618bbfe90bf32cd398adfe7156546
davidxby0 <davidxby0@gmail.com>
2017-06-17 16:26:14 -0400

    Update util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -62,6 +62,8 @@ bool HexString::Wipe() {
 
 std::string HexString::toString() {
 		
+    if (!IsData)
+	return "HexString is null";
     std::string output;
     output.reserve(2 * size);
     for (int i = 0; i < size; ++i)
